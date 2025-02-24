664c63f821b7dc21695149154b0afe02611939db
PedroOliDev <pedrooliveiraaguima@gmail.com>
2025-02-24 11:13:01 -0300

    ProgressbarUpdate

--- SiteLoja/Loja/loja.js
+++ SiteLoja/Loja/loja.js
@@ -1,34 +1,67 @@
+let total = 0;
 
-                // Adicione o código JavaScript para adicionar itens ao carrinho e calcular o total aqui
-                let total = 0
+function limparCarrinho() {
+    const carrinho = document.querySelector('.carrinho ul');
+    carrinho.innerHTML = '';
+    total = 0;
+    document.getElementById('totalcarrinho').textContent = total.toFixed(2);
+}
 
-                function limparCarrinho() {
-                    const carrinho = document.querySelector('.carrinho ul')
-                    carrinho.innerHTML = ''
-                    total = 0
-                    document.getElementById('totalcarrinho').textContent = total.toFixed(2)
-                }
+function adicionarItem(nome, preco) {
+    const carrinho = document.querySelector('.carrinho ul');
+    const novoItem = document.createElement('li');
+    novoItem.textContent = `${nome} - R$ ${preco}`;
+    carrinho.appendChild(novoItem);
 
+    total += preco;
+    document.getElementById('totalcarrinho').textContent = total.toFixed(2);
+}
 
-                function adicionarItem(nome, preco) {
-                    const carrinho = document.querySelector('.carrinho ul')
-                    const novoItem = document.createElement('li')
-                    novoItem.textContent = `${nome} - R$ ${preco}`
-                    carrinho.appendChild(novoItem)
+let interval;
 
-                    total += preco
-                    document.getElementById('totalcarrinho').textContent = total.toFixed(2)
-                }
+function finalizarCompra() {
+    if (total === 0) {
+        alert('Adicione itens ao carrinho antes de finalizar a compra!');
+        return;
+    } else {
+        iniciarProgresso();
+        
+    }
+}
 
-                function finalizarCompra() {
-                    if (total === 0) {
-                        alert('Adicione itens ao carrinho antes de finalizar a compra!')
-                        return
-                    }
-                    else {
-                        alert('Compra finalizada com sucesso!')
-                    }
-                
-                }
+function iniciarProgresso() {
+    clearInterval(interval);
+    
+    const valor1 = parseFloat(document.getElementById('preco1').value);
+    const valor2 = parseFloat(document.getElementById('preco2').value);
+    const valor3 = parseFloat(document.getElementById('preco3').value);
+
+    
+    const totalProgress = valor3 - valor1; 
+
+    const progressBar = document.getElementById('progress-bar');
+    progressBar.style.display = 'block'; 
+    let atual = 0; 
+
+    
+    progressBar.style.width = '0%';
+    progressBar.textContent = '0%';
+
+    interval = setInterval(() => {
+        
+        atual += totalProgress / 100; 
+
+        if (atual >= totalProgress) {
+            atual = totalProgress;
+            clearInterval(interval); 
+            alert('Compra finalizada com sucesso!');
+        }
 
         
+        const percent = ((atual / totalProgress) * 100).toFixed(2);
+
+        
+        progressBar.style.width = percent + '%';
+        progressBar.textContent = percent + '%';
+    }, 50);
+}
